6861a0bb3046e469b3ecaf912ebce24c6980fd2a
Alex Medvedev <medvedev@signatec.ru>
2019-09-10 08:43:54 +0700

    refactoring

--- BooksStore/BooksStore/Startup.cs
+++ BooksStore/BooksStore/Startup.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BooksStore.Models;
+﻿using BooksStore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace BooksStore
 {
