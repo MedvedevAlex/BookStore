99f0364724173a20ee1d78703f54bcf28e62f09f
Alex Medvedev <localisseat@gmail.com>
2019-09-23 20:30:28 +0700

    author fluentvalidation

---
+++ BooksStore/WebClient/FluentValidation/Models/CreateAuthorValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using InterfaceDB.Models;
+
+namespace WebClient.FluentValidation.Models
+{
+    public class CreateAuthorValidatior : AbstractValidator<Author>
+    {
+        public CreateAuthorValidatior()
+        {
+            RuleFor(b => b.Name).NotEmpty()
+                .WithMessage("Имя не может быть пустым");
+            RuleFor(b => b.Name).MaximumLength(20)
+                .WithMessage("Название не может превышать 30 символов");
+            RuleFor(b => b.Age).NotEmpty()
+                .WithMessage("Возраст не может быть пустым");
+            //RuleFor(b => b.Age).InclusiveBetween(1, 90)
+            //    .WithMessage("Название не может превышать 30 символов")
+        }
+    }
+}
