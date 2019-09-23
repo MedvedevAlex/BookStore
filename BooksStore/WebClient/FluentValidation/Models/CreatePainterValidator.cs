3c925425bf551e31cb196f64aa95495e454e6cbc
Alex Medvedev <localisseat@gmail.com>
2019-09-23 20:50:13 +0700

    painter fluentvalidation

---
+++ BooksStore/WebClient/FluentValidation/Models/CreatePainterValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using InterfaceDB.Models;
+
+namespace WebClient.FluentValidation.Models
+{
+    public class CreatePainterValidatior : AbstractValidator<Painter>
+    {
+        public CreatePainterValidatior()
+        {
+            RuleFor(p => p.Name).NotEmpty()
+                .WithMessage("Имя не может быть пустым");
+            RuleFor(p => p.Name).MaximumLength(20)
+                .WithMessage("Название не может превышать 30 символов");
+            RuleFor(p => p.Age).NotEmpty()
+                .WithMessage("Возраст не может быть пустым");
+            //RuleFor(p => p.Age).InclusiveBetween(1, 90)
+            //    .WithMessage("Название не может превышать 30 символов")
+            RuleFor(p => p.Style).IsInEnum()
+                .WithMessage("Стиль имеет диапазон значений, который не включает данное значение");
+        }
+    }
+}
