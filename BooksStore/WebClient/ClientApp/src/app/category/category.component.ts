07b25431f98a33ce44f290c957d835dced2bfaf4
butenko <butenko@signatec.ru>
2019-10-01 11:36:12 +0700

    commit

--- BooksStore/WebClient/ClientApp/src/app/category/category.component.ts
+++ BooksStore/WebClient/ClientApp/src/app/category/category.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
+  book: any[];
 
-  constructor() { }
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<any[]>('http://localhost:5000/api/Book/GetBooks').subscribe(result => {
+      this.book = result;
+      console.log('5555555555555', result);
+
+    }, error => console.error(error));
+  }
 
   ngOnInit() {
+
   }
 
 }
