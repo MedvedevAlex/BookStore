5b08177702a84540e46a64ec8ae922ba173a194e
butenko <butenko@signatec.ru>
2019-10-01 12:34:16 +0700

    codereview

--- BooksStore/WebClient/ClientApp/src/app/category/category.component.ts
+++ BooksStore/WebClient/ClientApp/src/app/category/category.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-category',
@@ -7,14 +6,9 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-  book: any[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<any[]>('http://localhost:5000/api/Book/GetBooks').subscribe(result => {
-      this.book = result;
-      console.log('5555555555555', result);
+  constructor() {
 
-    }, error => console.error(error));
   }
 
   ngOnInit() {
--- BooksStore/WebClient/ClientApp/src/app/home/home.component.ts
+++ BooksStore/WebClient/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,20 +10,5 @@ export class HomeComponent {
   public book: any[];
 
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<any[]>('http://localhost:5000/api/Book/GetBooks').subscribe(result => {
-      this.book = result;
-      console.log('5555555555555', result);
-      
-    }, error => console.error(error));
-
-    http.get<string[]>(baseUrl + 'api/SampleData/GetText').subscribe(result => {
-      this.text = result;
-    }, error => console.error(error));
-
-    http.get<any[]>(baseUrl + 'api/Book').subscribe(result => {
-      this.book = result;
-    }, error => console.error(error));
-    
-  }
+  constructor() { }
 }
