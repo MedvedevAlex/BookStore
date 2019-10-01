93d8b99c823783efb89cd90074623576e4155987
butenko <butenko@signatec.ru>
2019-10-01 13:09:39 +0700

    codereview

--- BooksStore/WebClient/ClientApp/src/app/shared/services/settings-helper.service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SettingsHelperService } from './settings-helper.service';
-
-describe('SettingsHelperService', () => {
-  it('should create an instance', () => {
-  //  expect(new SettingsHelperService()).toBeTruthy();
-  });
-});
