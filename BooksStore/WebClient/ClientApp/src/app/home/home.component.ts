e36f29275b191469fc06c0e5dcf203acde31b55d
butenko <butenko@signatec.ru>
2019-10-01 12:30:39 +0700

    first version of http helper

--- BooksStore/WebClient/ClientApp/src/app/home/home.component.ts
+++ BooksStore/WebClient/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { UrlInfoService } from '../shared/services/url-info.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(private url: UrlInfoService) {
+    url.getBookUrlAsync('GetBooks');
+  }
 }
---
+++ BooksStore/WebClient/ClientApp/src/app/shared/services/url-info.service.spec.ts
@@ -0,0 +1,7 @@
+import { UrlInfoService } from './url-info.service';
+
+describe('UrlInfoService', () => {
+  it('should create an instance', () => {
+  //  expect(new UrlInfoService()).toBeTruthy();
+  });
+});
