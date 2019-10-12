65613c98ad571d246adbb9d6fed683e634da4bb0
Alex Medvedev <localisseat@gmail.com>
2019-10-12 23:01:38 +0700

    add publishercontroller

---
+++ BooksStore/API/Controllers/PublisherController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.EntityService.PainterRepos;
+using API.EntityService.PublisherRepos;
+using InterfaceDB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class PublisherController : Controller
+    {
+        private readonly IPublisherRepository _publisherRepository;
+
+        public PublisherController(IPublisherRepository publisherRepository)
+        {
+            _publisherRepository = publisherRepository;
+        }
+
+        [HttpGet("SearchByPublishers")]
+        public async Task<ICollection<Publisher>> SearchByPublishersAsync([FromQuery]string searchString)
+        {
+            return await _publisherRepository.SearchByPublishersAsync(searchString);
+        }
+    }
+}
