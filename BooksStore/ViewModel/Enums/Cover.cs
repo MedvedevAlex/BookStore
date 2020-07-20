13922a41df50c9b7e7ddcb95c52c7179394f7d3d
AlexandrMedvedev <localisseat@gmail.com>
2020-07-20 15:38:57 +0700

    rename projects
    rename InterfaceDB to Model
    rename DBLayerAPI to Service
    add project ViewModel
    moved enums and models for ViewModel
    add summary and descriptions for enums

---
+++ BooksStore/ViewModel/Enums/Cover.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace ViewModel.Enums
+{
+    /// <summary>
+    /// Переплет
+    /// </summary>
+    public enum Cover
+    {
+        [Description("Мягкий")]
+        Paperback = 0,
+        [Description("Твердый")]
+        Hardback = 1
+    }
+}
