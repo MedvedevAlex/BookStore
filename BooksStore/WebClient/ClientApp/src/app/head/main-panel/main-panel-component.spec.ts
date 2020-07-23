453c5fc0774f7698d0fef75255d88e0f8a94a2b0
AlexandrMedvedev <localisseat@gmail.com>
2020-07-23 11:21:15 +0700

    delete front

--- BooksStore/WebClient/ClientApp/src/app/head/main-panel/main-panel-component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MainPanelComponent } from './main-panel-component';
-
-describe('MainPanelComponent', () => {
-  it('should create an instance', () => {
-    expect(new MainPanelComponent()).toBeTruthy();
-  });
-});
--- BooksStore/WebClient/ClientApp/src/app/shared/material-block-module.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MaterialBlockModule } from './material-block-module';
-
-describe('MaterialBlockModule', () => {
-  it('should create an instance', () => {
-    expect(new MaterialBlockModule()).toBeTruthy();
-  });
-});
