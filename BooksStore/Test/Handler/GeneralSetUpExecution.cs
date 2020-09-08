06c2d7af39e0793b5f16d2be6148b4199ca2150d
AlexandrMedvedev <localisseat@gmail.com>
2020-09-08 09:00:43 +0700

    configure tests
    before each start of tests create database
    before each end of tests delete database

---
+++ BooksStore/Test/Handler/GeneralSetUpExecution.cs
@@ -0,0 +1,38 @@
+﻿using Model;
+using NUnit.Framework;
+
+namespace Test.Handler
+{
+    /// <summary>
+    /// Класс отвечающий за настройку тестов перед и после выполнения
+    /// </summary>
+    [SetUpFixture]
+    public class GeneralSetUpExecution
+    {
+        /// <summary>
+        /// Метод создающий базу данных перед началом выполнения
+        /// всех тестов
+        /// </summary>
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            using (var context = new BookContextFactory().CreateDbContext(new string[0]))
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        /// <summary>
+        /// Метод удаляющий базу данных после выполнения
+        /// всех тестов
+        /// </summary>
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            using (var context = new BookContextFactory().CreateDbContext(new string[0]))
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
